f2bdaf731afc5a4c4bfb3f50aeb1747d53840d02
habibialaouired <redouane.habibi-alaoui@rte-france.com>
2020-01-14 15:11:10 +0100

    abonnement au deux topics

--- src/main/java/com/rte_france/plasma/hackathon/listener/MeterReadingListener.java
+++ src/main/java/com/rte_france/plasma/hackathon/listener/MeterReadingListener.java
@@ -3,6 +3,7 @@ package com.rte_france.plasma.hackathon.listener;
 import com.rte_france.plasma.hackathon.rest.MeterReadingController;
 import org.opensmartgridplatform.adapter.kafka.MeterReading;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -14,8 +15,9 @@ public class MeterReadingListener {
     @Autowired
     private MeterReadingController controller;
 
-    @org.springframework.kafka.annotation.KafkaListener(topics = "${spring.kafka.consumer.topics}",groupId = "${spring.kafka.consumer.group-id}" )
-    public void read(MeterReading meterReading){
+    @KafkaListener(topics = "${topics.meter-reading-power}",groupId = "${spring.kafka.consumer.group-id}" )
+    public void readMeterReadingPower(MeterReading meterReading){
+        System.out.println("meter-reading-power : " + meterReading);
         SseEmitter latestEm = controller.getLatestEmitter();
         try {
             if(latestEm != null){
@@ -26,5 +28,16 @@ public class MeterReadingListener {
         }
     }
 
-
+    @KafkaListener(topics = "${topics.meter-reading-internal-temp}",groupId = "${spring.kafka.consumer.group-id}" )
+    public void readmeterReadingInternalTemp(MeterReading meterReading){
+        System.out.println("meter-reading-internal-temp : " + meterReading);
+        SseEmitter latestEm = controller.getLatestEmitter();
+        try {
+            if(latestEm != null){
+                latestEm.send(meterReading.toString());
+            }
+        } catch (IOException e) {
+            latestEm.completeWithError(e);
+        }
+    }
 }
