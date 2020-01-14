4c62a6034074d488c6da377d07ca88ac457f9079
habibialaouired <redouane.habibi-alaoui@rte-france.com>
2020-01-14 13:19:53 +0100

    changement de nom de topic

--- src/main/java/com/rte_france/plasma/hackathon/config/KafkaConsumerConfiguration.java
+++ src/main/java/com/rte_france/plasma/hackathon/config/KafkaConsumerConfiguration.java
@@ -35,7 +35,7 @@ public class KafkaConsumerConfiguration {
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,KafkaAvroDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
         props.put("schema.registry.url", schemaRegistry);
         //props.put("specific.avro.reader", specificAvroReader);
